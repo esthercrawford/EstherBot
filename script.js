64a2a5ecb1ed559f9219a99bd8da08bbd197e746
DRocampomarcelo <marceloderada@gmail.com>
2016-04-21 18:57:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('So you want to learn about Marcelo? Just say HOLA to get started.')
                 .then(() => 'speak');
         }
     },
