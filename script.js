f89830baa8ed11fcadb404b6cf4b949f3334f5a0
kendoggz <kendoggz@users.noreply.github.com>
2016-04-25 21:42:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('So you want to learn about Ken? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                    return bot.say(`I didn't understand that. Remember, I'm not a smart bot`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
