feb2760f269e8a4976b5b8a3833faa0bcf206e17
leitrim <leitrim@gmail.com>
2016-05-31 22:15:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Joe's New ChatBot? Well thats me. Just say HELLO to get started.')
+            return bot.say('So you want to learn about Joes New ChatBot? Well thats me. Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
