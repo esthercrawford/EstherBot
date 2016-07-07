7a6847e4aa0cc8df4526995dda8f94df86fea9f2
Maria Li <marialilokyee@gmail.com>
2016-07-07 11:42:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Get started by saying HI.')
+            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
