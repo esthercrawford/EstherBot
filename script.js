dec95f81362724e3cf0f1e90c253a29b9af996a5
David Lira <dgdelira@gmail.com>
2016-10-05 22:35:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,37 @@
 'use strict';
 
-const _ = require('lodash');
 const Script = require('smooch-bot').Script;
-
-const scriptRules = require('./script.json');
 
 module.exports = new Script({
     processing: {
-        //prompt: (bot) => bot.say('Beep boop...'),
+        prompt: (bot) => bot.say('Beep boop...'),
         receive: () => 'processing'
     },
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
-                .then(() => 'speak');
+            return bot.say('Hi! I\'m DavidBot!')
+                .then(() => 'askName');
         }
     },
 
-    speak: {
+    askName: {
+        prompt: (bot) => bot.say('What\'s your name?'),
         receive: (bot, message) => {
+            const name = message.text;
+            return bot.setProp('name', name)
+                .then(() => bot.say(`Great! I'll call you ${name}
+Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
+                .then(() => 'finish');
+        }
+    },
 
-            let upperText = message.text.trim().toUpperCase();
-
-            function updateSilent() {
-                switch (upperText) {
-                    case "CONNECT ME":
-                        return bot.setProp("silent", true);
-                    case "DISCONNECT":
-                        return bot.setProp("silent", false);
-                    default:
-                        return Promise.resolve();
-                }
-            }
-
-            function getSilent() {
-                return bot.getProp("silent");
-            }
-
-            function processMessage(isSilent) {
-                if (isSilent) {
-                    return Promise.resolve("speak");
-                }
-
-                if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
-                }
-
-                var response = scriptRules[upperText];
-                var lines = response.split('\n');
-
-                var p = Promise.resolve();
-                _.each(lines, function(line) {
-                    line = line.trim();
-                    p = p.then(function() {
-                        console.log(line);
-                        return bot.say(line);
-                    });
-                })
-
-                return p.then(() => 'speak');
-            }
-
-            return updateSilent()
-                .then(getSilent)
-                .then(processMessage);
+    finish: {
+        receive: (bot, message) => {
+            return bot.getProp('name')
+                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+                        'teach me how to do anything else!'))
+                .then(() => 'finish');
         }
     }
 });
